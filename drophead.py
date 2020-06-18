659c9025f103deff797497d20b78a8636782243c
kirill-kravtsov <alterkirill2@gmail.com>
2020-06-18 12:11:35 +0200

    beautify & add comments

--- drophead.py
+++ drophead.py
@@ -1,26 +1,28 @@
-from transformers import BertModel, RobertaModel, XLMRobertaModel
 import torch
 import torch.nn.functional as F
+from transformers import BertModel, RobertaModel, XLMRobertaModel
 
 
 VALID_CLS = (BertModel, RobertaModel, XLMRobertaModel)
 
 
-def drophead_hook(module, input, output):
+def _drophead_hook(module, input, output):
+    """
+    Pytorch forward hook for transformers.modeling_bert.BertSelfAttention layer
+    """
     if (not module.training) or (module.p_drophead==0):
         return output
 
-    batch_size = output[0].shape[0]
-    dist = torch.distributions.Bernoulli(torch.tensor([1-module.p_drophead]))
-    mask = dist.sample((batch_size, module.num_attention_heads))
-    mask = mask.to(output[0].device).unsqueeze(-1)
-    count_ones = mask.sum(dim=1).unsqueeze(-1)
-
     orig_shape = output[0].shape
-    self_att_res = module.transpose_for_scores(output[0])
-    self_att_res = self_att_res * mask * module.num_attention_heads / count_ones
-    self_att_res = self_att_res.permute(0, 2, 1, 3).view(*orig_shape)
-    return (self_att_res,) + output[1:]
+    dist = torch.distributions.Bernoulli(torch.tensor([1-module.p_drophead]))
+    mask = dist.sample((orig_shape[0], module.num_attention_heads))
+    mask = mask.to(output[0].device).unsqueeze(-1)
+    count_ones = mask.sum(dim=1).unsqueeze(-1)  # calc num of active heads
+
+    self_att_out = module.transpose_for_scores(output[0])
+    self_att_out = self_att_out * mask * module.num_attention_heads / count_ones
+    self_att_out = self_att_out.permute(0, 2, 1, 3).view(*orig_shape)
+    return (self_att_out,) + output[1:]
 
 
 def valid_type(obj):
@@ -29,7 +31,9 @@ def valid_type(obj):
 
 def get_base_model(model):
     """
-    Check model type. If not correct then try to find in attributes,
+    Check model type. If correct then return the model itself.
+    If not correct then try to find in attributes and return correct type
+    attribute if found
     """
     if not valid_type(model):
         attrs = [name for name in dir(model) if valid_type(getattr(model, name))]
@@ -46,6 +50,7 @@ def set_drophead(model, p=0.1):
         model: an instance of transformers.BertModel / transformers.RobertaModel /
             transformers.XLMRobertaModel or downstream model (e.g. transformers.BertForSequenceClassification)
             or any custom downstream model
+        p: drophead probability
     """
     if (p < 0) or (p > 1):
         raise ValueError("Wrong p argument")
@@ -54,5 +59,5 @@ def set_drophead(model, p=0.1):
 
     for bert_layer in model.encoder.layer:
         if not hasattr(bert_layer.attention.self, "p_drophead"):
-            bert_layer.attention.self.register_forward_hook(drophead_hook)
+            bert_layer.attention.self.register_forward_hook(_drophead_hook)
         bert_layer.attention.self.p_drophead = p
